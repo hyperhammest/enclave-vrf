0a4f18b4d45b6261052478110860a9e3ed320efc
hero <staroomoostar@gmail.com>
2024-01-29 09:51:17 +0800

    Adapt for new keygrantor

--- go.mod
+++ go.mod
@@ -3,13 +3,13 @@ module github.com/smartbch/enclave-vrf
 go 1.18
 
 require (
-	github.com/ecies/go/v2 v2.0.5
-	github.com/edgelesssys/ego v0.4.1
+	github.com/edgelesssys/ego v1.2.0
 	github.com/ethereum/go-ethereum v1.11.0
-	github.com/tyler-smith/go-bip32 v1.0.0
-	github.com/vechain/go-ecvrf v0.0.0-20200326080414-5b7e9ee61906
 	github.com/smartbch/egvm v0.0.0-20230906040909-2bfbba098907
+	github.com/vechain/go-ecvrf v0.0.0-20220525125849-96fa0442e765
 )
+
+replace github.com/smartbch/egvm => ./../../hyperhammest/egvm
 
 require (
 	github.com/FactomProject/basen v0.0.0-20150613233007-fe3947df716e // indirect
@@ -18,18 +18,20 @@ require (
 	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
 	github.com/deckarep/golang-set/v2 v2.1.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
+	github.com/ecies/go/v2 v2.0.5 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/tklauser/go-sysconf v0.3.10 // indirect
 	github.com/tklauser/numcpus v0.4.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
+	github.com/tyler-smith/go-bip32 v1.0.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
 )
 
 require (
 	github.com/btcsuite/btcd v0.22.0-beta
-	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
 	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
 )
--- sgx-rand/rand.go
+++ sgx-rand/rand.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"crypto/ecdsa"
-	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
 	"flag"
@@ -10,15 +8,10 @@ import (
 	"net/http"
 	"os"
 	"sync"
-	"time"
 
-	secp256k1 "github.com/btcsuite/btcd/btcec"
 	"github.com/edgelesssys/ego/ecrypto"
-	"github.com/edgelesssys/ego/enclave"
 	"github.com/smartbch/egvm/keygrantor"
 	vrf "github.com/vechain/go-ecvrf"
-
-	"github.com/smartbch/enclave-vrf/sgx-rand/utils"
 )
 
 // #include "util.h"
@@ -31,95 +24,44 @@ type vrfResult struct {
 
 var listenURL string
 var keyGrantorUrl string
-var vrfPubkey []byte //compressed pubkey
-var vrfPrivKey *secp256k1.PrivateKey
 
-var blockHash2Beta map[string][]byte = make(map[string][]byte)
-var blockHash2PI map[string][]byte = make(map[string][]byte)
-var blockHash2Timestamp map[string]uint64 = make(map[string]uint64)
+var randClient *keygrantor.SimpleClient
+
+var blockHash2Beta = make(map[string][]byte)
+var blockHash2PI = make(map[string][]byte)
+var blockHash2Timestamp = make(map[string]uint64)
 var blockHashSet []string
 var lock sync.RWMutex
 
-var keyFile = "/data/key.txt"
-
-const serverName = "SGX-VRF-PUBKEY"
-const attestationProviderURL = "https://shareduks.uks.attest.azure.net"
+const (
+	maxBlockHashCount = 5000
+	serverName        = "SGX-VRF-PUBKEY"
+	// IntelCPUFreq /proc/cpuinfo model name
+	intelCPUFreq = 2800_000000
+	keyFile      = "/data/key.txt"
+)
 
 // start slave first, then start master to send key to them
 // master must be sure slave is our own enclave app
 // slave no need to be sure master is enclave app because the same vrf pubkey provided by all slave and master owned, it can check outside.
 func main() {
 	initConfig()
-	recoveryPrivateKeyFromFile()
-	go createAndStartHttpsServer()
-	select {}
-}
-
-func initConfig() {
-	keyGrantorUrlP := flag.String("g", "0.0.0.0:8084", "keygrantor url")
-	listenURLP := flag.String("l", "0.0.0.0:8082", "listen address")
-	flag.Parse()
-	listenURL = *listenURLP
-	fmt.Println(listenURL)
-	keyGrantorUrl = *keyGrantorUrlP
-}
-
-func generateRandom32Bytes() []byte {
-	var out []byte
-	var x C.uint16_t
-	var retry C.int = 1
-	for i := 0; i < 32; i++ {
-		C.rdrand_16(&x, retry)
-		out = append(out, byte(x))
-	}
-	return out
-}
-
-// IntelCPUFreq /proc/cpuinfo model name
-const intelCPUFreq = 2800_000000
-
-func getTimestampFromTSC() uint64 {
-	cycleNumber := uint64(C.get_tsc())
-	return cycleNumber / intelCPUFreq
-}
-
-func createAndStartHttpsServer() {
-	// Create a TLS config with a self-signed certificate and an embedded report.
-	//tlsCfg, err := enclave.CreateAttestationServerTLSConfig()
-	cert, _, tlsCfg := utils.CreateCertificate(serverName)
-	certHash := sha256.Sum256(cert)
-
-	// init handler for remote attestation
-	http.HandleFunc("/cert", func(w http.ResponseWriter, r *http.Request) { w.Write([]byte(hex.EncodeToString(cert))) })
-	http.HandleFunc("/peer-report", func(w http.ResponseWriter, r *http.Request) {
-		peerReport, err := enclave.GetRemoteReport(certHash[:])
-		if err != nil {
+	randClient = &keygrantor.SimpleClient{}
+	_, err := os.ReadFile(keyFile)
+	if err != nil {
+		if os.IsNotExist(err) {
+			randClient.InitKeys(keyGrantorUrl, [32]byte{}, false)
+			fmt.Printf("get enclave vrf private key from keygrantor, its pubkey is: %s\n", hex.EncodeToString(randClient.PubKeyBz))
+			sealKeyToFile()
+		} else {
 			panic(err)
 		}
-		w.Write([]byte(hex.EncodeToString(peerReport)))
-	})
-
-	initVrfHttpHandlers()
-
-	server := http.Server{Addr: listenURL, TLSConfig: &tlsCfg, ReadTimeout: 3 * time.Second, WriteTimeout: 5 * time.Second}
-	fmt.Println("listening ...")
-	err := server.ListenAndServeTLS("", "")
-	fmt.Println(err)
-}
-
-func initVrfHttpHandlers() {
-	// look up secp256k1 pubkey
-	http.HandleFunc("/pubkey", func(w http.ResponseWriter, r *http.Request) {
-		if len(vrfPubkey) == 0 {
-			return
-		}
-		w.Write([]byte(hex.EncodeToString(vrfPubkey)))
-		return
-	})
-
-	// not check the block hash correctness
-	http.HandleFunc("/blockhash", func(w http.ResponseWriter, r *http.Request) {
-		if len(vrfPubkey) == 0 {
+	} else {
+		randClient.InitKeys(keyFile, [32]byte{}, true)
+	}
+	handlers := make(map[string]func(w http.ResponseWriter, r *http.Request))
+	handlers["/blockhash"] = func(w http.ResponseWriter, r *http.Request) {
+		if len(randClient.PubKeyBz) == 0 {
 			return
 		}
 		hash := r.URL.Query()["b"]
@@ -138,7 +80,7 @@ func initVrfHttpHandlers() {
 		if err != nil {
 			return
 		}
-		beta, pi, err := vrf.NewSecp256k1Sha256Tai().Prove((*ecdsa.PrivateKey)(vrfPrivKey), hashBytes)
+		beta, pi, err := vrf.Secp256k1Sha256Tai.Prove(randClient.PrivKey.ToECDSA(), hashBytes)
 		if err != nil {
 			fmt.Printf("do vrf failed: %s\n", err.Error())
 			w.Write([]byte(err.Error()))
@@ -150,9 +92,8 @@ func initVrfHttpHandlers() {
 		blockHashSet = append(blockHashSet, blkHash)
 		clearOldBlockHash()
 		fmt.Printf("%v sent block hash to me %v\n", r.RemoteAddr, r.URL.Query()["b"])
-	})
-
-	http.HandleFunc("/vrf", func(w http.ResponseWriter, r *http.Request) {
+	}
+	handlers["/vrf"] = func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("%v sent block hash to get vrf %v\n", r.RemoteAddr, r.URL.Query()["b"])
 		hash := r.URL.Query()["b"]
 		if len(hash) == 0 {
@@ -178,36 +119,24 @@ func initVrfHttpHandlers() {
 		out, _ := json.Marshal(res)
 		w.Write(out)
 		return
-	})
-
-	http.HandleFunc("/report", func(w http.ResponseWriter, r *http.Request) {
-		if len(vrfPubkey) == 0 {
-			return
-		}
-		hash := sha256.Sum256(vrfPubkey)
-		report, err := enclave.GetRemoteReport(hash[:])
-		if err != nil {
-			w.Write([]byte(err.Error()))
-			return
-		}
-		w.Write([]byte(hex.EncodeToString(report)))
-	})
-
-	// send jwt token
-	http.HandleFunc("/token", func(w http.ResponseWriter, r *http.Request) {
-		if len(vrfPubkey) == 0 {
-			return
-		}
-		token, err := enclave.CreateAzureAttestationToken(vrfPubkey, attestationProviderURL)
-		if err != nil {
-			w.Write([]byte(err.Error()))
-			return
-		}
-		w.Write([]byte(token))
-	})
+	}
+	go randClient.CreateAndStartHttpsServer(serverName, listenURL, handlers)
+	select {}
 }
 
-const maxBlockHashCount = 5000
+func initConfig() {
+	keyGrantorUrlP := flag.String("g", "0.0.0.0:8084", "keygrantor url")
+	listenURLP := flag.String("l", "0.0.0.0:8082", "listen address")
+	flag.Parse()
+	listenURL = *listenURLP
+	fmt.Println(listenURL)
+	keyGrantorUrl = *keyGrantorUrlP
+}
+
+func getTimestampFromTSC() uint64 {
+	cycleNumber := uint64(C.get_tsc())
+	return cycleNumber / intelCPUFreq
+}
 
 func clearOldBlockHash() {
 	nums := len(blockHashSet)
@@ -223,41 +152,8 @@ func clearOldBlockHash() {
 	}
 }
 
-func getKeyFromKeyGrantor() {
-	outKey, err := keygrantor.GetKeyFromKeyGrantor(keyGrantorUrl, [32]byte{})
-	if err != nil {
-		fmt.Println("failed to deserialize the key from server")
-		panic(err)
-	}
-	vrfPrivKey, _ = secp256k1.PrivKeyFromBytes(secp256k1.S256(), outKey.Key)
-	vrfPubkey = vrfPrivKey.PubKey().SerializeCompressed()
-	fmt.Printf("get enclave vrf private key from keygrantor, its pubkey is: %s\n", hex.EncodeToString(vrfPubkey))
-	sealKeyToFile()
-	return
-}
-
-func recoveryPrivateKeyFromFile() {
-	fileData, err := os.ReadFile(keyFile)
-	if err != nil {
-		fmt.Printf("read file failed, %s\n", err.Error())
-		if os.IsNotExist(err) {
-			// maybe first run this enclave app
-			getKeyFromKeyGrantor()
-		}
-		return
-	}
-	rawData, err := ecrypto.Unseal(fileData, nil)
-	if err != nil {
-		fmt.Printf("unseal file data failed, %s\n", err.Error())
-		return
-	}
-	vrfPrivKey, _ = secp256k1.PrivKeyFromBytes(secp256k1.S256(), rawData)
-	vrfPubkey = vrfPrivKey.PubKey().SerializeCompressed()
-	fmt.Println("recover vrf keys")
-}
-
 func sealKeyToFile() {
-	out, err := ecrypto.SealWithUniqueKey(vrfPrivKey.Serialize(), nil)
+	out, err := ecrypto.SealWithUniqueKey(randClient.PrivKey.Serialize(), nil)
 	if err != nil {
 		panic(err)
 	}
